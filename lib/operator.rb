7e131255044ed3bb52a43213a12f48f562a8a5f3
postazure <gordonmaxc@gmail.com>
2014-11-06 09:39:30 -0800

    stage

--- lib/operator.rb
+++ lib/operator.rb
@@ -1,32 +1,4 @@
 module Rover
   class Operator
-    def initialize location
-      @xpos, @ypos, face = location.split("")
-
-      @plateau = Plateau.new
-
-      direction = {'N' => :north, 'E' => :east, 'S' => :south, 'W' => :west}[face]
-      coordinate = Coordinate.new(@xpos.to_i, @ypos.to_i)
-      @cart = Cart.new(coordinate, direction, @plateau)
-    end
-
-    def plateau
-      @plateau
-    end
-
-    def cart
-      @cart
-    end
-
-    def run_command cmd
-      cmd.each_char do |char|
-        @cart.run_command(char)
-      end
-    end
-
-    def reset location
-      Operator.new(location)
-    end
-
   end
 end
